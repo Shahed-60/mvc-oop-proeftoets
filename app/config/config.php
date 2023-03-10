36011e269ba74cf91a8a3bee180155d816c5ca3d
Shahed Amer <chanyeol6521354@gmail.com>
2023-03-10 23:41:41 +0100

    bijna alles is af

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'Mvc-2209C');
+// define('DB_NAME', 'Mvc-2209C');
+define('DB_NAME', 'mvc-oop-proeftoets');
+
 // echo DB_HOST;
